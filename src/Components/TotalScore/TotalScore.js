8fb2f2ba0949b6fec60e74116ea800f53e339c9a
Patrick Alvarez Eades <p.alvarezeades@gmail.com>
2023-11-14 14:12:52 +0100

    Total Score visual edit

--- src/Components/TotalScore/TotalScore.js
+++ src/Components/TotalScore/TotalScore.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { initialGames } from '../../data';
+import './TotalScore.css';
 
 function TotalScore() {
   return (
-    <div>
+    <div className='totalScore'> 
         <h4>Total Score:</h4>
         <ul>
             {initialGames.map(game => <li key={game}>{game}:</li>)}
